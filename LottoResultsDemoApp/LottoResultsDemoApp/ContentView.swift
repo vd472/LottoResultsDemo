89ea5916496195d33f9c294e820481556e15f7df
vd472 <das.vijayesh@gmail.com>
2025-09-29 13:48:30 +0200

    iOS: initial project setup for LottoResultsDemoApp

---
+++ LottoResultsDemoApp/LottoResultsDemoApp/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  LottoResultsDemoApp
+//
+//  Created by vijayesha on 29.09.25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LottoResultsDemoApp/LottoResultsDemoApp/LottoResultsDemoAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  LottoResultsDemoAppApp.swift
+//  LottoResultsDemoApp
+//
+//  Created by vijayesha on 29.09.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LottoResultsDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
