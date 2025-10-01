10a58e050eb9959a00f66a2bc7ebb6de6ac5e76a
vd472 <das.vijayesh@gmail.com>
2025-10-01 05:06:26 +0200

    iOS: create widget extension for the app

---
+++ LottoResultsDemoApp/LottoResultsWidget/LottoResultsWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  LottoResultsWidgetBundle.swift
+//  LottoResultsWidget
+//
+//  Created by vijayesha on 01.10.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LottoResultsWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LottoResultsWidget()
+        LottoResultsWidgetControl()
+    }
+}
